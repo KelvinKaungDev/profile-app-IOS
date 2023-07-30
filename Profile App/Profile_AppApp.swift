852feaeaec591c6a704f09776f8ac20ef4c412c4
Kelvin Gao <u6511753@au.edu>
2023-07-30 23:42:31 +0700

    Initial Commit

---
+++ Profile App/Profile_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Profile_AppApp.swift
+//  Profile App
+//
+//  Created by Kelvin Gao  on 30/7/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct Profile_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
