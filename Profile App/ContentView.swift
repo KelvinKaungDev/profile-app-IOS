93121c1f36b882c772db26210a0c6e1f19d8b42e
Kelvin Gao <u6511753@au.edu>
2023-07-31 00:48:32 +0700

    Finished by using SwiftUI

--- Profile App/ContentView.swift
+++ Profile App/ContentView.swift
@@ -1,21 +1,105 @@
-//
-//  ContentView.swift
-//  Profile App
-//
-//  Created by Kelvin Gao  on 30/7/2566 BE.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                VStack {
+                   Image("profile")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .background(.black)
+                        .frame(width: 200, height: 200, alignment: .center)
+                        .clipShape(Circle())
+                        .padding(.top, 40)
+                    
+                    Text("高明志 \n IOS Developer")
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .multilineTextAlignment(.center)
+                        .padding(.top, 20)
+                }
+    
+                VStack {
+                    HStack(spacing: 35){
+                        Image(systemName: "heart.circle")
+                            .resizable()
+                            .foregroundColor(.red)
+                            .aspectRatio(contentMode: .fit)
+                        
+                        Image(systemName: "network")
+                            .resizable()
+                            .foregroundColor(.red)
+                            .aspectRatio(contentMode: .fit)
+                        
+                        Image(systemName: "message.circle")
+                            .resizable()
+                            .foregroundColor(.red)
+                            .aspectRatio(contentMode: .fit)
+                        
+                        Image(systemName: "phone.circle")
+                            .resizable()
+                            .foregroundColor(.red)
+                            .aspectRatio(contentMode: .fit)
+                    }
+                    .padding(50)
+                    
+                    HStack (spacing: 40) {
+                        VStack {
+                            Text("1775")
+                                .foregroundColor(.red)
+                                .font(.title3)
+                                .fontWeight(.bold)
+                            Text("Applicants")
+                                .foregroundColor(.black)
+                                .fontWeight(.bold)
+                                .font(.body)
+                        }
+                        
+                        VStack {
+                            Text("20031")
+                                .foregroundColor(.red)
+                                .font(.title3)
+                                .fontWeight(.bold)
+                            Text("Followers")
+                                .foregroundColor(.black)
+                                .fontWeight(.bold)
+                        }
+
+                        VStack {
+                            Text("21001")
+                                .foregroundColor(.red)
+                                .font(.title3)
+                                .fontWeight(.bold)
+                            Text("Subscribers")
+                                .foregroundColor(.black)
+                                .fontWeight(.bold)
+                        }
+
+                    }
+                    
+                    RoundedRectangle(cornerRadius: 120)
+                        .frame(width: 200, height: 60, alignment: .center)
+                        .foregroundColor(.red)
+                        .overlay(
+                            Text("Follow")
+                                .foregroundColor(.white)
+                                .font(.title2)
+                                .fontWeight(.bold)
+                        )
+                        .padding(.top, 60)
+
+                    Spacer()
+
+                }
+                .padding(.top)
+            }
         }
-        .padding()
     }
 }
 
